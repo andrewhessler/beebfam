abf88d24fce198fc52c34e2b7fe7013d5b80c9c1
Andrew Hessler <adhessler@gmail.com>
2025-10-28 18:56:59 -0500

    these have to be stacked for mobile

--- grocery-list/src/client/src/App.tsx
+++ grocery-list/src/client/src/App.tsx
@@ -113,7 +113,7 @@ function App() {
   return (
     <div id="content">
       <div id="input">
-        <select onChange={(event) => setCategory(event.target.value)}>
+        <select id="input-select" onChange={(event) => setCategory(event.target.value)}>
           {CATEGORIES.map((cat) =>
             <option value={cat}>{cat}</option>
           )}
