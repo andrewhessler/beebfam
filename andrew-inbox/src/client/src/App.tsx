0d7ed82e3c7176f189bc609be4415b2bfd891d7c
Andrew Hessler <adhessler@gmail.com>
2025-10-27 20:47:57 -0500

    nope doesn't work

--- andrew-inbox/src/client/src/App.tsx
+++ andrew-inbox/src/client/src/App.tsx
@@ -6,35 +6,6 @@ type Item = {
   name: string,
 }
 
-// ios workaround
-// https://github.com/WebKit/WebKit/pull/2907#issuecomment-2204513113
-function focusAndOpenKeyboard(el: HTMLInputElement, timeout: number) {
-  if (!timeout) {
-    timeout = 100;
-  }
-  if (el) {
-    // Align temp input element approximately where the input element is
-    // so the cursor doesn't jump around
-    const __tempEl__ = document.createElement('input');
-    __tempEl__.style.position = 'absolute';
-    __tempEl__.style.top = (el.offsetTop + 7) + 'px';
-    __tempEl__.style.left = el.offsetLeft + 'px';
-    __tempEl__.style.height = "0";
-    __tempEl__.style.opacity = "0";
-    // Put this temp element as a child of the page <body> and focus on it
-    document.body.appendChild(__tempEl__);
-    __tempEl__.focus();
-
-    // The keyboard is open. Now do a delayed focus on the target element
-    setTimeout(function() {
-      el.focus();
-      el.click();
-      // Remove the temp element
-      document.body.removeChild(__tempEl__);
-    }, timeout);
-  }
-}
-
 function App() {
   const [items, setItems] = useState<Item[]>([]);
   const [newItem, setNewItem] = useState<string>("");
@@ -88,9 +59,9 @@ function App() {
 
   useEffect(() => {
     if (inputRef.current) {
-      focusAndOpenKeyboard(inputRef.current, 100);
+      (inputRef.current as HTMLInputElement).focus()
     }
-  }, [])
+  })
   return (
     <div id="items">
       <input ref={inputRef} type="text" value={newItem} onKeyDown={addItem} onChange={(event) => setNewItem(event.target.value)} />
