5204eea53ad868a41e8dec37f7ded9bf23814668
Andrew Hessler <adhessler@gmail.com>
2025-12-31 10:15:12 -0600

    add exercises

---
+++ exercise-tracker/migrations/20251231161232_extra_exercises.sql
@@ -0,0 +1,23 @@
+INSERT INTO anaerobic_template (name, weight, sets, reps)
+VALUES ("hammer curls", NULL, NULL, NULL);
+
+INSERT INTO anaerobic_template (name, weight, sets, reps)
+VALUES ("reverse fly", NULL, NULL, NULL);
+
+INSERT INTO anaerobic_template (name, weight, sets, reps)
+VALUES ("reverse dumbbell curl", NULL, NULL, NULL);
+
+INSERT INTO anaerobic_template (name, weight, sets, reps)
+VALUES ("shrugs", NULL, NULL, NULL);
+
+INSERT INTO anaerobic_template (name, weight, sets, reps)
+VALUES ("skull crushers", NULL, NULL, NULL);
+
+INSERT INTO anaerobic_template (name, weight, sets, reps)
+VALUES ("overhead extension", NULL, NULL, NULL);
+
+INSERT INTO anaerobic_template (name, weight, sets, reps)
+VALUES ("upright row", NULL, NULL, NULL);
+
+INSERT INTO anaerobic_template (name, weight, sets, reps)
+VALUES ("lateral raise", NULL, NULL, NULL);
