85c33cfde69dffd4f60b3b0ed52aaa0d2775d265
Andrew Hessler <adhessler@gmail.com>
2026-01-03 10:36:43 -0600

    add filter

--- exercise-tracker/src/client/src/Heatmap.tsx
+++ exercise-tracker/src/client/src/Heatmap.tsx
@@ -1,5 +1,48 @@
+import { useEffect, useState } from 'react';
+
+type ExerciseType = "anaerobic" | "aerobic";
+
+type ExerciseTemplate = {
+  name: string,
+  type: ExerciseType,
+  category: string,
+  duration_min?: number | string,
+  distance?: number | string,
+  weight?: number | string,
+  sets?: number | string,
+  reps?: number | string,
+}
+
 function Heatmap() {
-  return <div></div>;
+  const [exerciseTemplates, setExerciseTemplates] = useState<ExerciseTemplate[]>([]);
+  const [currentExercise, setCurrentExercise] = useState<string>("");
+
+  useEffect(() => {
+    async function getTemplates() {
+      const response = await fetch("/get-templates");
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      const { templates }: { templates: ExerciseTemplate[] } = await response.json();
+      setExerciseTemplates(templates);
+      if (templates.length > 0) {
+        setCurrentExercise(templates[0].name);
+      }
+    }
+    getTemplates();
+  }, []);
+
+  return (
+    <div>
+      <select value={currentExercise} onChange={(event) => setCurrentExercise(event.target.value)}>
+        {exerciseTemplates.map((template) => (
+          <option key={template.name} value={template.name}>
+            {template.name}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
 }
 
 export default Heatmap;
