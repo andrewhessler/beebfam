f23089643b644b6004ff33a5eab91c670892961c
Andrew Hessler <adhessler@gmail.com>
2026-01-08 12:33:20 -0600

    add pinch exercises

---
+++ exercise-tracker/migrations/20260108183253_more_exercises.sql
@@ -0,0 +1,5 @@
+INSERT INTO anaerobic_template (name, weight, sets, reps)
+VALUES ("two finger pinch", NULL, NULL, NULL);
+
+INSERT INTO anaerobic_template (name, weight, sets, reps)
+VALUES ("thumb pinch", NULL, NULL, NULL);
